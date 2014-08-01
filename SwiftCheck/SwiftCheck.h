e1dc7b4a0c4dbe78d2242b2694dbb0b44cba06de
CodaFi <devteam.codafi@gmail.com>
2014-07-31 20:25:14 -0600

    Initial Commit

---
+++ SwiftCheck/SwiftCheck.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCheck.h
+//  SwiftCheck
+//
+//  Created by Robert Widmann on 7/31/14.
+//  Copyright (c) 2014 Robert Widmann. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftCheck.
+FOUNDATION_EXPORT double SwiftCheckVersionNumber;
+
+//! Project version string for SwiftCheck.
+FOUNDATION_EXPORT const unsigned char SwiftCheckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCheck/PublicHeader.h>
+
+
