76ce718d2e1fc80f82774637b8ff72e9ed4e77bf
Robert Widmann <devteam.codafi@gmail.com>
2014-11-04 08:25:10 -0700

    Mac Scheme

--- SwiftCheck/SwiftCheck.h
+++ SwiftCheck/SwiftCheck.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Robert Widmann. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftCheck.
 FOUNDATION_EXPORT double SwiftCheckVersionNumber;
