c42e18c68f6deb458afe6e9c3b78773da82b072d
Robert Widmann <devteam.codafi@gmail.com>
2015-01-29 19:21:27 -0700

    Test modifiers

---
+++ SwiftCheckTests/ModifierSpec.swift
@@ -0,0 +1,30 @@
+//
+//  ModifierSpec.swift
+//  SwiftCheck
+//
+//  Created by Robert Widmann on 1/29/15.
+//  Copyright (c) 2015 Robert Widmann. All rights reserved.
+//
+
+import XCTest
+import SwiftCheck
+
+class ModifierSpec : XCTestCase {
+	func testModifiers() {
+		property["All blind variables print '(*)'"] = forAll { (x : Blind<Int>) in
+			return x.description == "(*)"
+		}
+		
+		property["Static propositions never shrink"] = forAll { (x : Static<Int>) in
+			return Static<Int>.shrink(x).isEmpty
+		}
+
+		property["Positive propositions only generate positive numbers"] = forAll { (x : Positive<Int>) in
+			return x.getPositive >= 0
+		}
+		
+		property["NonZero propositions never generate zero"] = forAll { (x : NonZero<Int>) in
+			return x.getNonZero != 0
+		}
+	}
+}
