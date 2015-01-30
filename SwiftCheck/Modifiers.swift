95e6d173625669c0e822f1878335ed4950bcd28b
Robert Widmann <devteam.codafi@gmail.com>
2015-01-29 19:21:21 -0700

    Make Modifiers Printable

--- SwiftCheck/Modifiers.swift
+++ SwiftCheck/Modifiers.swift
@@ -9,7 +9,7 @@
 import Swiftz
 
 public struct Blind<A : Arbitrary> : Arbitrary, Printable {
-	let getBlind : A
+	public let getBlind : A
 	
 	public init(_ blind : A) {
 		self.getBlind = blind
@@ -32,39 +32,55 @@ public struct Blind<A : Arbitrary> : Arbitrary, Printable {
 	}
 }
 
+public func == <T : protocol<Arbitrary, Equatable>>(lhs : Blind<T>, rhs : Blind<T>) -> Bool {
+	return lhs.getBlind == rhs.getBlind
+}
+
 public func <^><A : Arbitrary, B : Arbitrary>(f: A -> B, ar : Blind<A>) -> Blind<B> {
 	return Blind<B>(f(ar.getBlind))
 }
 
-public struct Fixed<A : Arbitrary> : Arbitrary {
-	let getFixed : A
+public struct Static<A : Arbitrary> : Arbitrary, Printable {
+	public let getStatic : A
 	
 	public init(_ fixed : A) {
-		self.getFixed = fixed
+		self.getStatic = fixed
 	}
 	
-	private static func create(blind : A) -> Fixed<A> {
-		return Fixed(blind)
+	public var description : String {
+		return "Static( \(self.getStatic) )"
 	}
 	
-	public static func arbitrary() -> Gen<Fixed<A>> {
-		return Fixed.create <^> A.arbitrary()
+	private static func create(blind : A) -> Static<A> {
+		return Static(blind)
 	}
 	
-	public static func shrink(bl : Fixed<A>) -> [Fixed<A>] {
+	public static func arbitrary() -> Gen<Static<A>> {
+		return Static.create <^> A.arbitrary()
+	}
+	
+	public static func shrink(bl : Static<A>) -> [Static<A>] {
 		return []
 	}
 }
 
-public func <^><A : Arbitrary, B : Arbitrary>(f: A -> B, ar : Fixed<A>) -> Fixed<B> {
-	return Fixed<B>(f(ar.getFixed))
+public func == <T : protocol<Arbitrary, Equatable>>(lhs : Static<T>, rhs : Static<T>) -> Bool {
+	return lhs.getStatic == rhs.getStatic
 }
 
-public struct Positive<A : protocol<Arbitrary, SignedNumberType>> : Arbitrary {
-	let getPositive : A
+public func <^><A : Arbitrary, B : Arbitrary>(f: A -> B, ar : Static<A>) -> Static<B> {
+	return Static<B>(f(ar.getStatic))
+}
+
+public struct Positive<A : protocol<Arbitrary, SignedNumberType>> : Arbitrary, Printable {
+	public let getPositive : A
 	
 	public init(_ pos : A) {
 		self.getPositive = pos
+	}
+	
+	public var description : String {
+		return "Positive( \(self.getPositive) )"
 	}
 	
 	private static func create(blind : A) -> Positive<A> {
@@ -72,7 +88,7 @@ public struct Positive<A : protocol<Arbitrary, SignedNumberType>> : Arbitrary {
 	}
 	
 	public static func arbitrary() -> Gen<Positive<A>> {
-		return ((Positive.create • abs) <^> A.arbitrary().suchThat(!=0)).suchThat({ $0.getPositive > 0 })
+		return (Positive.create • abs) <^> A.arbitrary().suchThat(>=0)
 	}
 	
 	public static func shrink(bl : Positive<A>) -> [Positive<A>] {
@@ -80,15 +96,23 @@ public struct Positive<A : protocol<Arbitrary, SignedNumberType>> : Arbitrary {
 	}
 }
 
+public func == <T : protocol<Arbitrary, SignedNumberType>>(lhs : Positive<T>, rhs : Positive<T>) -> Bool {
+	return lhs.getPositive == rhs.getPositive
+}
+
 public func <^><A : protocol<Arbitrary, SignedNumberType>, B : protocol<Arbitrary, SignedNumberType>>(f: A -> B, ar : Positive<A>) -> Positive<B> {
 	return Positive<B>(f(ar.getPositive))
 }
 
-public struct NonZero<A : protocol<Arbitrary, IntegerType>> : Arbitrary {
-	let getNonZero : A
+public struct NonZero<A : protocol<Arbitrary, IntegerType>> : Arbitrary, Printable {
+	public let getNonZero : A
 	
 	public init(_ non : A) {
 		self.getNonZero = non
+	}
+	
+	public var description : String {
+		return "NonZero( \(self.getNonZero) )"
 	}
 	
 	private static func create(blind : A) -> NonZero<A> {
@@ -96,12 +120,18 @@ public struct NonZero<A : protocol<Arbitrary, IntegerType>> : Arbitrary {
 	}
 	
 	public static func arbitrary() -> Gen<NonZero<A>> {
-		return NonZero.create <^> A.arbitrary().suchThat(!=0)
+		return NonZero.create <^> A.arbitrary().suchThat({ x in 
+			x != 0 
+		})
 	}
 	
 	public static func shrink(bl : NonZero<A>) -> [NonZero<A>] {
 		return A.shrink(bl.getNonZero).filter(!=0).map({ NonZero($0) })
 	}
+}
+
+public func == <T : protocol<Arbitrary, IntegerType>>(lhs : NonZero<T>, rhs : NonZero<T>) -> Bool {
+	return lhs.getNonZero == rhs.getNonZero
 }
 
 public func <^><A : protocol<Arbitrary, IntegerType>, B : protocol<Arbitrary, IntegerType>>(f: A -> B, ar : NonZero<A>) -> NonZero<B> {
