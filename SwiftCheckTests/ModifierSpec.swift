a00f27498e7df6191727b616367d0c7c6b29e678
Robert Widmann <devteam.codafi@gmail.com>
2015-05-29 22:31:03 -0600

    Test modifiers

--- SwiftCheckTests/ModifierSpec.swift
+++ SwiftCheckTests/ModifierSpec.swift
@@ -41,5 +41,13 @@ class ModifierSpec : XCTestCase {
 				^&&^
 				(xs.getArray == xs.getArray.reverse().reverse()) <?> "Right identity"
 		}
+
+		property["dictionaryof"] = forAll { (d : DictionaryOf<String, Int>) in
+			return true
+		}
+
+		property["setOf"] = forAll { (s : SetOf<Int>) in
+			return true
+		}
 	}
 }
