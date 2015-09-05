bf016fe4e805175e57c9fa839a3dcc2e33fa38ed
Robert Widmann <devteam.codafi@gmail.com>
2015-09-05 15:05:39 -0400

    Pre-requisites

--- Tutorial.playground/Contents.swift
+++ Tutorial.playground/Contents.swift
@@ -3,5 +3,23 @@
 import SwiftCheck
 import XCTest
 
+//: # Prerequisites
 
+//: This tutorial assumes that you have a fairly good grasp on Swift, its syntax, and terms like 
+//: 
+//: * [(Data) Type](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html)
+//: * [Protocol-Oriented Programming](https://developer.apple.com/videos/wwdc/2015/?id=408)
+//: * [Test-Driven Development](https://en.wikipedia.org/wiki/Test-driven_development)
+//: * [Polymorphism](https://en.wikipedia.org/wiki/Polymorphism)
+//: * [Map/Filter/Reduce](https://medium.com/@ivicamil/higher-order-functions-in-swift-part-1-d8e75f963d13)
+//:
+//: What this tutorial does *not* require are that you know
+//:
+//: * [Abstract Nonsense](https://en.wikipedia.org/wiki/Abstract_nonsense)
+//: * Functional Programming
+//: * What that dumb 'M' word is/does/means
+
+//: # Introduction
+
+//:
 
