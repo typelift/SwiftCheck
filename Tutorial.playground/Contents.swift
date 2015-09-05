556d9f0aad8f6f4f580dc81361136963297087be
Robert Widmann <devteam.codafi@gmail.com>
2015-09-05 17:39:38 -0400

    Write an introduction

--- Tutorial.playground/Contents.swift
+++ Tutorial.playground/Contents.swift
@@ -5,7 +5,7 @@ import XCTest
 
 //: # Prerequisites
 
-//: This tutorial assumes that you have a fairly good grasp on Swift, its syntax, and terms like 
+//: This tutorial assumes that you have a fairly good grasp of Swift, its syntax, and terms like
 //: 
 //: * [(Data) Type](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html)
 //: * [Protocol-Oriented Programming](https://developer.apple.com/videos/wwdc/2015/?id=408)
@@ -21,5 +21,15 @@ import XCTest
 
 //: # Introduction
 
-//:
+//: SwiftCheck is a testing library that augments libraries like `XCTest` and `Quick`, by giving them
+//: the ability to automatically test program properties.  A property is a particular facet of an
+//: algorithm, method, data structure, or program that must *hold* (that is, remain valid) even when
+//: fed random or pseudo-random data.  If that all seems complicated, it may be simpler to think of
+//: Property Testing like Fuzz Testing, but with the outcome being to satisfy requirements rather than
+//: break the program.  Throughout this tutorial, simplifications like the above will be made to aid
+//: your understanding.  Towards the end of it, we will begin to remove much of these "training wheels"
+//: and reveal the real types of the operations in the library, which are often much more powerful and
+//: generic than previously presented.
+
+
 
