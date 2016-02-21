cdec5b741b2dad80cf20033884b2e8af630c52dd
Robert Widmann <devteam.codafi@gmail.com>
2016-02-21 13:29:40 -0500

    Remove Operadics dependency in package manager

--- Package.swift
+++ Package.swift
@@ -1,9 +1,4 @@
 import PackageDescription
 
-let package = Package(
-    name: "SwiftCheck",
-    dependencies: [
-        .Package(url: "https://github.com/typelift/Operadics.git", Version(0, 2, 0)),
-    ]
-)
+let package = Package(name: "SwiftCheck")
 
