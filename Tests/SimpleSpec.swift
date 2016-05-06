1e0ff5da4d3378bdbfbb91b4f749042153e5acb6
Robert Widmann <devteam.codafi@gmail.com>
2016-05-06 19:17:00 -0400

    Declare an unreasonably large struct example test

--- Tests/SimpleSpec.swift
+++ Tests/SimpleSpec.swift
@@ -24,6 +24,49 @@ extension ArbitraryFoo : Arbitrary {
 	}
 }
 
+public struct ArbitraryLargeFoo {
+	let a : Int8
+	let b : Int16
+	let c : Int32
+	let d : Int64
+	let e : UInt8
+	let f : UInt16
+	let g : UInt32
+	let h : UInt64
+	let i : Int
+	let j : UInt
+	let k : Bool
+	let l : (Bool, Bool)
+	let m : (Bool, Bool, Bool)
+	let n : (Bool, Bool, Bool, Bool)
+}
+
+extension ArbitraryLargeFoo : Arbitrary {
+	public static var arbitrary : Gen<ArbitraryLargeFoo> {
+		return Gen<(Int8, Int16, Int32, Int64
+				  , UInt8, UInt16, UInt32, UInt64
+				  , Int , UInt)>
+			.zip( Int8.arbitrary
+				, Int16.arbitrary
+				, Int32.arbitrary
+				, Int64.arbitrary
+				, UInt8.arbitrary
+				, UInt16.arbitrary
+				, UInt32.arbitrary
+				, UInt64.arbitrary
+				, Int.arbitrary
+				, UInt.arbitrary).flatMap { t in
+			return Gen<(Bool, (Bool, Bool), (Bool, Bool, Bool), (Bool, Bool, Bool, Bool))>
+				.zip( Bool.arbitrary
+					, Gen<(Bool, Bool)>.zip(Bool.arbitrary, Bool.arbitrary)
+					, Gen<(Bool, Bool, Bool)>.zip(Bool.arbitrary, Bool.arbitrary, Bool.arbitrary)
+					, Gen<(Bool, Bool, Bool, Bool)>.zip(Bool.arbitrary, Bool.arbitrary, Bool.arbitrary, Bool.arbitrary)).map({ t2 in
+						return (t.0, t.1, t.2, t.3, t.4, t.5, t.6, t.7, t.8, t.9, t2.0, t2.1, t2.2, t2.3)
+					}).map(ArbitraryLargeFoo.init)
+		}
+	}
+}
+
 class SimpleSpec : XCTestCase {
 	func testAll() {
 		property("Integer Equality is Reflexive") <- forAll { (i : Int8) in
