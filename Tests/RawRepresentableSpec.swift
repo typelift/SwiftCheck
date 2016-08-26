63a0c00f80df3f0502e7f37d8e3e08d62d321d30
Robert Widmann <devteam.codafi@gmail.com>
2016-08-25 23:31:31 -0700

    Restrict RawRepresentable extensions to Integers
    It is dangerous to do otherwise.  The implementations for Strings and
    FloatingPoint numbers have incredibly poor performance because they
    cannot guess the user’s intent.  The String implementation will most
    certainly not construct english words that could ever initialize
    anything.  The FloatingPoint implementation will never guess the right
    combination of bits that will produce the “exact float” - whatever that
    could mean - to match the initializer expression.

--- Tests/RawRepresentableSpec.swift
+++ Tests/RawRepresentableSpec.swift
@@ -15,27 +15,21 @@ enum ImplicitRawValues : Int {
 	case baz
 }
 
-// Declaring the extension allows Swift to know this particular enum can be Arbitrary
-// ...but it doesn't need to be implemented since the protocol extension gives us a default implementation!
-extension ImplicitRawValues: Arbitrary {}
+extension ImplicitRawValues : Arbitrary {}
 
-enum ExplicitRawValues : Int {
+enum ExplicitRawIntValues : Int {
 	case zero = 0
 	case one = 1
 	case two = 2
 }
 
-class RawRepresentable_ArbitrarySpec: XCTestCase {
-	func testDefaultRawRepresentableGeneratorWithImplicitRawValues() {
+class RawRepresentableSpec: XCTestCase {
+	func testRepresentable() {
 		property("only generates Foo, Bar, or Baz") <- forAll { (e: ImplicitRawValues) in
 			return [.foo, .bar, .baz].contains(e)
 		}
-	}
 
-	func testDeafultRawRepresentableGeneratorWithExplicitRawValues() {
-		// when no extension is given, the user has to call `forAllNoShrink` since the compiler doesn't automatically
-		// infer protocol conformance
-		property("only generates Zero, One, or Two") <- forAllNoShrink(ExplicitRawValues.arbitrary) { e in
+		property("only generates Zero, One, or Two") <- forAllNoShrink(ExplicitRawIntValues.arbitrary) { e in
 			return [.zero, .one, .two].contains(e)
 		}
 	}
