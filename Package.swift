5002b1ecf4607975d47618b3c0bb86daec9a6c12
Robert Widmann <devteam.codafi@gmail.com>
2018-01-05 16:51:19 -0700

    Formalize re-org

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["SwiftCheck"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/trill-lang/FileCheck.git", from: "0.0.3")
+		.package(url: "https://github.com/llvm-swift/FileCheck.git", from: "0.0.3")
 	],
 	targets: [
 		.target(
