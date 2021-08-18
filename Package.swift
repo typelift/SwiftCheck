87a56f050073f4ff5737f8535cf476c326d85f27
Guang1234567 <lihansey163@163.com>
2021-08-18 19:24:44 +0800

    [IDE] Make breakpoint  works in debug mode with CLion.

--- Package.swift
+++ Package.swift
@@ -3,21 +3,27 @@
 import PackageDescription
 
 let package = Package(
-	name: "SwiftCheck",
-	products: [
-		.library(
-			name: "SwiftCheck",
-			targets: ["SwiftCheck"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/llvm-swift/FileCheck.git", from: "0.1.0")
-	],
-	targets: [
-		.target(
-			name: "SwiftCheck"),
-		.testTarget(
-			name: "SwiftCheckTests",
-			dependencies: ["SwiftCheck", "FileCheck"]),
-	]
+        name: "SwiftCheck",
+        products: [
+            .library(
+                    name: "SwiftCheck",
+                    targets: ["SwiftCheck"]),
+        ],
+        dependencies: [
+            .package(url: "https://github.com/llvm-swift/FileCheck.git", from: "0.1.0"),
+        ],
+        targets: [
+            .target(
+                    name: "SwiftCheck"),
+            .testTarget(
+                    name: "SwiftCheckTests",
+                    dependencies: ["SwiftCheck", "FileCheck", ],
+                    linkerSettings: [
+                        .unsafeFlags(["-Xlinker", "-rpath",
+                                      "-Xlinker", "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/usr/lib"],
+                                .when(platforms: [.macOS])
+                        ),
+                    ]),
+        ]
 )
 
